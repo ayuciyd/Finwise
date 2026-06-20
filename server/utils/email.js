fb78446d955a88b68c6289239a9921605a19c0e1
Ayushi Yadav <your-github-yadavayushi1439@gmail.com>
2026-06-21 01:09:17 +0530

    fix: restore global IPv4 DNS order and configure mailer timeouts

--- server/utils/email.js
+++ server/utils/email.js
@@ -6,7 +6,10 @@ const transporter = nodemailer.createTransport({
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
-  }
+  },
+  connectionTimeout: 10000, // 10 seconds
+  greetingTimeout: 10000,
+  socketTimeout: 10000
 });
 
 const sendEmail = async ({ to, subject, html }) => {
